642392e9ba533ed9da4d620a56017a59e5687846
hrusha <bagaevvald@outlook.com>
2018-10-16 23:57:38 +0500

    last

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import {AuthComponent} from './auth.component';
 import {CommonModule} from '@angular/common';
+import {AuthRoutingModule} from './auth-routing.module';
 
 @NgModule({
   declarations: [LoginComponent, RegistrationComponent, AuthComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, AuthRoutingModule]
 })
 
 export class AuthModule {}
