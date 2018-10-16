48ac8d02177f62581999ecb41c262e1028f863f6
hrusha <bagaevvald@outlook.com>
2018-10-16 23:57:38 +0500

    Регистрация роутов + подключение шаблона

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {AuthModule} from './auth/auth.module';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {AuthModule} from './auth/auth.module';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AuthModule
+    AuthModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
