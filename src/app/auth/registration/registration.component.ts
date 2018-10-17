b8bfd9515b94d7fa7620798a205edc8cb330f2f5
Valeriy_V_Bagaev <v.bagaev@sarrc.ru>
2018-10-17 14:33:55 +0400

    Создание асинхронного валидатора

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -17,7 +17,7 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
         'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
         'name': new FormControl(null, [Validators.required]),
         'agree': new FormControl(false, [Validators.requiredTrue]),
@@ -36,6 +36,19 @@ export class RegistrationComponent implements OnInit {
           }})
       });
 
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.usersService.getUserByEmail(control.value)
+        .subscribe((user: User) => {
+          if (user) {
+            resolve({forbiddenEmail: true});
+          } else {
+            resolve(null);
+          }
+        })
+    });
   }
 
 }
