6f96166d217094c85ace4c1840c8c3398d500a41
Valeriy_V_Bagaev <v.bagaev@sarrc.ru>
2018-10-17 14:11:44 +0400

    Страница регистрации

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,7 +4,7 @@ import {UsersService} from '../../shared/services/users.service';
 import {User} from '../../shared/models/user.module';
 import {Message} from '../../shared/models/message.model';
 import {AuthService} from '../../shared/services/auth.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'wfm-login',
@@ -16,19 +16,27 @@ export class LoginComponent implements OnInit {
   form: FormGroup;
   message: Message;
 
-  constructor(private usersUserservice: UsersService, private authService: AuthService, private router: Router) {
+  constructor(private usersUserservice: UsersService, private authService: AuthService, private router: Router, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.message = new Message('danger', '' )
+    this.message = new Message('danger', '');
+    this.route.queryParams
+      .subscribe((params: Params) => {
+        if (params['nowCanLoggin']) {
+          this.showMessage({text: 'Теперь Вы можете зайти в систему.', type: 'success'});
+        }
+      });
+
+
     this.form = new FormGroup({
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
     });
   }
 
-  private showMessage(text: string, type: string = 'danger') {
-    this.message = new Message(type, text);
+  private showMessage(message: Message) {
+    this.message = message;
     window.setTimeout(() => {
       this.message.text = '';
     }, 5000);
@@ -47,10 +55,10 @@ export class LoginComponent implements OnInit {
             this.authService.login();
             //this.router.navigate(['']);
           } else {
-            this.showMessage('Пароль не верный!');
+            this.showMessage({text: 'Пароль не верный!', type: 'danger'});
           }
         } else {
-          this.showMessage('Такого пользователя не существует');
+          this.showMessage({text: 'Такого пользователя не существует', type: 'danger'});
         }
       });
   }
--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {UsersService} from '../../shared/services/users.service';
+import {User} from '../../shared/models/user.module';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'wfm-registration',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private usersService: UsersService, private router: Router) {
+  }
 
   ngOnInit() {
+    this.form = new FormGroup({
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+        'name': new FormControl(null, [Validators.required]),
+        'agree': new FormControl(false, [Validators.requiredTrue]),
+      }
+    );
+  }
+
+  onSubmit() {
+    const {email, password, name} = this.form.value;
+    const user = new User(email, password, name);
+
+    this.usersService.createNewUser(user)
+      .subscribe(() => {
+        this.router.navigate(['/login'], {queryParams: {
+          nowCanLoggin: true
+          }})
+      });
+
   }
 
 }
