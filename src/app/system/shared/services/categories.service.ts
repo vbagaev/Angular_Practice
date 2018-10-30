d3f6eb775eb1dbae0d6bad895d50ceaf968b1fd2
Valeriy_V_Bagaev <v.bagaev@sarrc.ru>
2018-10-30 11:32:28 +0400

    Страница записей. Добавление категорий

---
+++ src/app/system/shared/services/categories.service.ts
@@ -0,0 +1,17 @@
+import {BaseApi} from '../core/base-api';
+import {Http} from '@angular/http';
+import {Injectable} from '@angular/core';
+import {Category} from '../models/category.model';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+export class CategoriesService  extends BaseApi{
+
+  constructor(public http: Http) {
+    super(http);
+  }
+
+  addCategory(category: Category): Observable<Category> {
+    return this.post('categories', category);
+  }
+}
--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -17,13 +17,14 @@ import {MomentPipe} from './shared/pipes/moment.pipe';
 import { AddEventComponent } from './records-page/add-event/add-event.component';
 import { AddCategoryComponent } from './records-page/add-category/add-category.component';
 import { EditCategoryComponent } from './records-page/edit-category/edit-category.component';
+import {CategoriesService} from './shared/services/categories.service';
 
 
 @NgModule({
   imports: [CommonModule, SharedModule, SystemRoutingModule],
   declarations: [BillPageComponent, PlanningPageComponent, RecordsPageComponent,
     HistoryPageComponent, SystemComponent, SidebarComponent, HeaderComponent, DropdownDirective, BillCardComponent, CurrencyCardComponent, MomentPipe, AddEventComponent, AddCategoryComponent, EditCategoryComponent],
-  providers: [BillService]
+  providers: [BillService, CategoriesService]
 })
 
 export class SystemModule {
